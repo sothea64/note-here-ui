e3831434997845a2600c38c265061c2325b86e3d
Chansothea Bo <chansotheabo@gmail.com>
2022-05-25 21:42:38 +0700

    try create login form

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Note Here</h1>
+        <form>
+          <label htmlFor="#username">Username: </label>
+          <input id="#username" type={"text"}></input>
+          <br/>
+          <label htmlFor="#password">Password: </label>
+          <input id="#password" type={"password"}></input>
+        </form>
       </header>
     </div>
   );
