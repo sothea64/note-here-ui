a2db8ed35f4435982c87e5bd8408b43fd708dc64
Chansothea Bo <chansotheabo@gmail.com>
2022-06-13 21:53:28 +0700

    working on main page

--- src/screens/main/Main.js
+++ src/screens/main/Main.js
@@ -6,6 +6,7 @@ import ThemeButton from "../../components/theme-button/ThemeButton";
 import {
   getRememberTheme,
   ThemeContext,
+  themes,
   themesJSX,
 } from "../../contexts/ThemeContext.js";
 
@@ -16,8 +17,12 @@ function Main(props) {
         {({ theme }) =>
           (theme === undefined && <h1>Loading...</h1>) || (
             <div>
-              <Navbar style={{ backgroundColor: themesJSX[theme].backgroundColor, border:"1px solid white" }} expand="lg">
-                <Container>
+              <Navbar
+                variant={theme === themes.dark ? "dark" : "light"}
+                style={{ border: "0px solid white", borderBottomWidth: "1px" }}
+                expand="lg"
+              >
+                <Container variant={theme === themes.dark ? "dark" : "light"}>
                   <Navbar.Brand>
                     <Link
                       style={{
@@ -29,26 +34,73 @@ function Main(props) {
                       Note Here
                     </Link>
                   </Navbar.Brand>
-                  <Navbar.Toggle style={{ color: themesJSX[theme].color }} aria-controls="basic-navbar-nav" />
-                  <Navbar.Collapse id="basic-navbar-nav">
+                  <Navbar.Toggle
+                    style={{ color: themesJSX[theme].color }}
+                    variant={theme === themes.dark ? "dark" : "light"}
+                    aria-controls="basic-navbar-nav"
+                  />
+                  <Navbar.Collapse
+                    variant={theme === themes.dark ? "dark" : "light"}
+                    id="basic-navbar-nav"
+                  >
                     <Nav className="me-auto">
                       <Nav.Link
                         style={{ color: themesJSX[theme].color }}
+                        sm={{ order: 1 }}
                         href="#note"
                       >
                         Note
                       </Nav.Link>
                       <Nav.Link
                         style={{ color: themesJSX[theme].color }}
+                        sm={{ order: 2 }}
                         href="#todo"
                       >
                         To-Do
                       </Nav.Link>
                     </Nav>
-                    <ThemeButton className="m-0 p-0" />
+                    <ThemeButton className="m-0 p-0 sm-order-0" />
                   </Navbar.Collapse>
                 </Container>
               </Navbar>
+              <div className="pt-2">
+                <section
+                  style={{
+                    height: "92vh",
+                    overflow: "hidden",
+                  }}
+                >
+                  <div
+                    className="col col-sm-12 col-md-3 col-lg-3 col-xlg-3"
+                    style={{
+                      height: "100%",
+                      padding: "1px",
+                      backgroundColor: "whitesmoke",
+                      border: "1px solid transparent",
+                      borderRadius: "5px"
+                    }}
+                  >
+                    <div
+                      className="card m-1"
+                      style={{
+                        backgroundColor: themesJSX[theme].backgroundColor,
+                        borderWidth: "0px",
+                      }}
+                    >
+                      <h1>1st Card</h1>
+                    </div>
+                    <div
+                      className="card m-1"
+                      style={{
+                        backgroundColor: themesJSX[theme].backgroundColor,
+                        borderWidth: "0px",
+                      }}
+                    >
+                      <h1>2nd Card</h1>
+                    </div>
+                  </div>
+                </section>
+              </div>
             </div>
           )
         }
